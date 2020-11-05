02ff8a28fd67b4b2452e399f5cd0a49fc85af480
mochrira <mochrira@gmail.com>
2020-11-05 21:02:33 +0700

    separate pengguna

--- src/Pengguna.php
+++ src/Pengguna.php
@@ -1,12 +1,63 @@
 <?php 
 
 namespace Selvi\Firebase;
-use Selvi\Route;
+use Selvi\Exception;
+use Selvi\Factory;
+use Selvi\Input;
+use Selvi\Database\Manager as Database;
+use Selvi\Firebase\Manager as FirebaseManager;
 
 class Pengguna {
 
-    public static function setup() {
-        Route::apiResource('pengguna', '\\Selvi\\Firebase\\Controllers\\PenggunaController', ['get', 'patch', 'delete']);
+    private static $schema;
+    private static $firebaseAuth;
+    private static $firebaseToken;
+    private static $penggunaAktif;
+
+    static function setup($config = []) {
+        if(isset($config['schema'])) {
+            self::$schema = Database::get($config['schema']);
+            self::$schema->addMigration(__DIR__.'/../migrations/pengguna');
+        }
+    }
+
+    static function validateToken() {
+        try {
+            $input = Factory::load(Input::class, [], 'input');
+            $token = $input->header('authorization');
+            if(!$token) {
+                $token = $input->header('Authorization');
+            }
+            self::$firebaseAuth = FirebaseManager::getFactory()->createAuth();
+            self::$firebaseToken = self::$firebaseAuth->verifyIdToken($token);
+        } catch(Exception $e) {
+            throw $e;
+        } catch(\Exception $e) {
+            throw new Exception($e->getMessage(), 'auth/invalid-token');
+        }
+    }
+
+    static function validatePengguna() {
+        try {
+            $uid = self::$firebaseToken->getClaim('sub');
+            self::$penggunaAktif = self::$schema->where([['uid', $uid]])->get('pengguna')->row();
+            if(!self::$penggunaAktif) {
+                $firebaseUser = self::$firebaseAuth->getUser($uid);
+                if(!self::$schema->insert('pengguna', [
+                    'uid' => $uid,
+                    'displayName' => $firebaseUser->displayName,
+                    'photoUrl' => $firebaseUser->photoUrl,
+                    'email' => $firebaseUser->email
+                ])) {
+                    Throw new Exception('Gagal menambahkan pengguna', 'firebase-auth/insert-failed', 500);
+                }
+                self::$penggunaAktif = self::$schema->where([['uid', $uid]])->get('pengguna')->row();
+            }
+        } catch(Exception $e) {
+            throw e;
+        } catch(\Exception $e) {
+            throw new Exception($e->getMessage(), 'auth/invalid-token');
+        }
     }
 
 }
